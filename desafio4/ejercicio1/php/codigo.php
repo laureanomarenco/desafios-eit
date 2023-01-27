9eaf644da4b6a7bf7a39a8b26af00943feb44564
Laureano Marenco <laureanomarenco@gmail.com>
2023-01-27 17:43:30 -0300

    desafio 4 completo

---
+++ desafio4/ejercicio1/php/codigo.php
@@ -0,0 +1,8 @@
+<?php
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+
+ echo 'Hola tu nombre es ' . $usuario . ' y tu contraseña es ' . $contraseña;
+?>
